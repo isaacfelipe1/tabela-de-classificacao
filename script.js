ee5d156b6cc58a138b36858e037a2ecb86527a07
isaacfelipe1 <62969919+isaacfelipe1@users.noreply.github.com>
2021-03-30 20:37:01 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,59 @@
+var paulo={
+    nome:"Paulo",
+    vitorias:2,
+    empates:5,
+    derrotas:1,
+    pontos: 0
+}
+var isaac={
+    nome:"Isaac",
+    vitorias:3,
+    empates:5,
+    derrotas:2,
+    pontos:0
+}
+isaac.pontos=calculaPontos(isaac)
+paulo.pontos=calculaPontos(paulo)
+function calculaPontos(jogador) {
+    var pontos=(jogador.vitorias *3) + jogador.empates
+    return pontos
+}
+
+var jogadores=[isaac,paulo]
+exibirJogadoresNaTela(jogadores)
+
+function exibirJogadoresNaTela(jogadores) {
+    var html=""
+    for (var i=0;i<jogadores.length;i++) {
+        html+="<tr><td>"+ jogadores[i].nome+"</td>"
+        html+="<td>" + jogadores[i].vitorias + "</td>"
+        html+="<td>" + jogadores[i].empates + "</td>"
+        html+="<td>" + jogadores[i].derrotas+ "</td>"
+        html+="<td>" + jogadores[i].pontos + "</td>"
+        html+="<td><button onclick='adicionarVitoria("+ i + ")'>Vitória</button></td>"
+        html+="<td><button onclick='adicionarEmpate("+ i + ")'>Empate</button></td>"
+        html+="<td><button onclick='adicionarDerrota("+ i + ")'>Derrota</button></td></tr>"
+       
+    }
+    var tabelaJogadores=document.getElementById('tabelaJogadores')
+    tabelaJogadores.innerHTML=html
+    
+}
+function adicionarVitoria(i) {
+    var jogador=jogadores[i]
+    jogador.vitorias++
+    jogador.pontos=calculaPontos(jogador)
+    exibirJogadoresNaTela(jogadores)
+}
+function adicionarEmpate(i) {
+    var jogador=jogadores[i]
+    jogador.empates++
+    jogador.pontos=calculaPontos(jogador)
+    exibirJogadoresNaTela(jogadores)
+}
+
+function adicionarDerrota(i) {
+    var jogador=jogadores[i]
+    jogador.derrotas++
+    exibirJogadoresNaTela(jogadores) 
+}
